41e3191ef9906bf15925b1f2dd7fedea28e5147b
raulodev <raulodev@gmail.com>
2025-06-26 09:09:00 -0400

    update setting

--- src/settings.py
+++ src/settings.py
@@ -3,3 +3,5 @@ from decouple import config
 EMAIL = config("EMAIL")
 PASSWORD = config("PASSWORD")
 SPREADSHEET_ID = config("SPREADSHEET_ID")
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+GOHIGHLEVEL_TOKEN = config("GOHIGHLEVEL_TOKEN")
